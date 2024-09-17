39e85311e8938949ea067973a367c62fde6a5d5b
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-09-17 09:35:38 -0400

    Converted game.html to jsx

---
+++ Fronted/src/components/GameContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function GameContainer() {
+  return (
+    <div
+      id="game_container"
+      className="border my-20 mx-auto bg-gray-500 w-10/12 min-h-[700px]"
+    ></div>
+  );
+}
+
+export default GameContainer;
--- Fronted/src/pages/GamePage.jsx
+++ Fronted/src/pages/GamePage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import GameContainer from "../components/GameContainer";
 
 function GamePage() {
-  return <div>GamePage</div>;
+  return (
+    <div>
+      <NavBar links={["Home", "Leaderboard", "Support", "Login/Signup"]} />
+      <GameContainer />
+      <Footer />
+    </div>
+  );
 }
 
 export default GamePage;
